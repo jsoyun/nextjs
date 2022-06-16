e257515d3465d67b377af36fd1e37379647b3048
jsoyun <syusmm233@gmail.com>
2022-06-16 22:46:57 +0900

    styled-component

--- app/components/Top.styled.tsx
+++ app/components/Top.styled.tsx
@@ -1,12 +1,11 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 export const TopContainer = styled.footer`
-position: fixed;
-bottom: 60px;
-left: 0;
-display: flex;
-justify-content: space-between;
-width: 100%;
-padding: 0 60px;
-background-color: black;
-
-`
+  position: fixed;
+  bottom: 60px;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 60px;
+  background-color: red;
+`;
