0b8359af427292258a28d0c8f153740d8decc4d1
jsoyun <syusmm233@gmail.com>
2022-06-24 23:59:00 +0900

    컴포넌트

--- app/components/styles/button/Button.tsx
+++ app/components/styles/button/Button.tsx
@@ -11,3 +11,6 @@ export const Button = ({ buttonType }: Props) => {
 const Btn = styled.button<Props>`
   background-color: red;
 `;
+
+//집에서 맥북들고 회사일하는 여자
+//왜 컴포넌트 적용이 안되고 있니 여기서
--- app/pages/Box.tsx
+++ app/pages/Box.tsx
@@ -2,25 +2,24 @@ import styled from "styled-components";
 
 const box = () => {
   return (
-    <>
-      <BoxContainer>
-        <Container>
-          <Text>css테스트</Text>
-        </Container>
-      </BoxContainer>
-    </>
+    <BoxContainer>
+      <Text style={{ color: "red" }}>css테스트</Text>
+    </BoxContainer>
   );
 };
 
 export default box;
-const BoxContainer = styled.div``;
+const BoxContainer = styled.div`
+  border: solid green 3px;
+`;
 const Container = styled.div`
   display: flex;
   justify-content: center;
   /* display: flex; */
 `;
-const Text = styled.div`
+const Text = styled.p`
   /* display: flex; */
   border: solid 4px blue;
   background-color: red;
+  color: blue;
 `;
